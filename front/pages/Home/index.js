475753db2683b444fe83b91838a42d0022ea3474
EnriqueMayaFernandez <102796618+EnriqueMayaFernandez@users.noreply.github.com>
2022-05-11 15:49:05 +0200

    Homepage
    Creamos la home page

---
+++ front/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navbar from "../../components/Navbar/index"
+import Footer from "../../components/Footer/index"
+
+export default function HomePage() {
+
+  return (
+    <>
+      <Navbar>
+
+      </Navbar>
+      <div className="App-wrapper">
+        <div className="App-main">
+        </div>
+      </div>
+      <Footer>
+
+      </Footer>
+    </>
+  )
+}
--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import HomePage from '../pages/Home/index'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage>
+        
+      </HomePage>
     </div>
   );
 }
